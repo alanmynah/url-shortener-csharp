96f6d93dbbc007ea08fbc9e5c0f41762b0a419eb
AM <michael.rybintsev@gmail.com>
2020-12-30 16:57:20 +0000

    /check/slug

--- url-shortener-csharp/Controllers/ShortLinkController.cs
+++ url-shortener-csharp/Controllers/ShortLinkController.cs
@@ -10,42 +10,49 @@ namespace url_shortener_csharp.Controllers
     public class ShortLinkController : ControllerBase
     {
         private readonly ILogger<ShortLinkController> _logger;
-        private readonly AppDbContext _db; // yep, just here in the controller. 
-                                            // i have heard of repositories and units of work, yes, but again, 
+        private readonly AppDbContext _db; // yep, just here in the controller.
+                                            // i have heard of repositories and units of work, yes, but again,
                                             // let's keep it simple
 
         public ShortLinkController(ILogger<ShortLinkController> logger, AppDbContext db)
         {
             _logger = logger;
-            _db = db; 
+            _db = db;
         }
 
+        // this one isn't going to age well if we have a lot of links
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             // bear with me, yes, awful, i know
-            var links = await _db.ShortLinks.ToListAsync(); 
+            var links = await _db.ShortLinks.ToListAsync();
             return Ok(links);
         }
-        
+
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            // bear with me, yes, awful, i know
             var link = await _db.ShortLinks.FirstOrDefaultAsync(sl => sl.Id == id);
 
             return Ok(link);
         }
 
-        
+        [HttpGet("/check/{slug}")]
+        public async Task<IActionResult> Get(string slug)
+        {
+            var link = await _db.ShortLinks.FirstOrDefaultAsync(sl => sl.Slug == slug);
+
+            return Ok(link);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] ShortLinkRequest linkRequest) // no DTO, because what for? too simple of an example
         {
             _logger.LogInformation("Creating new short URL");
-            
+
             if (linkRequest.Destination is null)
                 return BadRequest("All links require destination");
-            
+
             ShortLink link;
             if (linkRequest.Slug is null)
             {
@@ -54,13 +61,14 @@ namespace url_shortener_csharp.Controllers
             }
             else
             {
+                // not checking for slug collision just yet
                 link = new ShortLink(linkRequest.Slug, linkRequest.Destination);
-            } 
-            
+            }
+
             // save to db
             await _db.ShortLinks.AddAsync(link);
             await _db.SaveChangesAsync(); // lol, kind of love how awful this is.
-            // if this is shocking, do read the note at the top. 
+            // if this is shocking, do read the note at the top.
             // wonder how many calls would start causing issues with this
 
             return Ok(link);
