34a50595e14176b6e478ed3fa4cfe9c672b6c168
AM <michael.rybintsev@gmail.com>
2020-12-30 11:44:07 +0000

    scaffolding docker-compose for mssql

--- url-shortener-csharp/Models/ShortLink.cs
+++ url-shortener-csharp/Models/ShortLink.cs
@@ -9,7 +9,10 @@ namespace url_shortener_csharp
             Slug = slug;
             Destination = destination;
         }
-
+        
+        public int Id { get; set; } // tbh, slug could've been used as ID as it's intended to be unique,
+                                    // but i can't be bothered to think through why this is a bad idea now, 
+                                    // so just adding id to save some unexpected headache and get on with this example
         public string Slug { get; }
         public string Destination  { get; }
 
